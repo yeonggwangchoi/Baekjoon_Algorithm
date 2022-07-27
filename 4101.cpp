315027b2e4c1c3bc983befd72c527b72f73a9801
yeonggwangchoi <dudrhkd7811@naver.com>
2022-07-27 23:14:37 +0900

    220727 commit

---
+++ 4101.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2;
+	while (true)
+	{
+		cin >> num1 >> num2;
+		if (num1 == 0 && num2 == 0)
+			break;
+		else
+		{
+			if (num1 > num2)
+				cout << "Yes\n";
+			else
+				cout << "No\n";
+		}
+	}
+}
