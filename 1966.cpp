8aa211dc19008769bf94803dbea95edc9f3a1cc8
yeonggwangchoi <dudrhkd7811@naver.com>
2022-08-22 23:56:19 +0900

    220822 commit

--- 1966.cpp
+++ 1966.cpp
@@ -8,31 +8,6 @@ int main()
 	cin >> T;
 	for (int i = 0; i < T; i++)
 	{
-		int N, M;
-		cin >> N >> M;
-		queue <int> q;
-		int count = 0;
-		int max = 0;
-		int j;
-		int rem = 0;
-		for (j = 0; j < N; j++)
-		{
-			int q_num;
-			cin >> q_num;
-			if (q_num > max)
-				max = q_num;
-			q.push(q_num);
-			
-		}
-		for (int k = 0; k < q.size(); k++)
-		{
-			if (q.front()< max)
-			{
-				q.push(q.front());
-				q.pop();
-				count++;
-			}
-		}
-		cout <<count <<"\n";
+		
 	}
 }
