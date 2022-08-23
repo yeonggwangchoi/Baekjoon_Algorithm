857199132fd0e015b54f3d73d1b1bce07e82d50e
yeonggwangchoi <dudrhkd7811@naver.com>
2022-08-23 16:21:30 +0900

    220823 commit

--- 2108.cpp
+++ 2108.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	int N;
 	cin >> N;
 	vector <int> v;
-
+	int num[8001] = {};
+	
 	for (int i = 0; i < N; i++)
 	{
 		int num;
@@ -16,20 +21,34 @@ int main()
 		v.push_back(num);
 	}
 	sort(v.begin(), v.end());
-	int sum = 0;
+	double sum = 0;
 	for (int i = 0; i < v.size(); i++)
 	{
 		sum += v[i];
+		num[v[i] + 4000]++;
 	}
-	
-	cout << "\n";
-	if (sum / N < 0)
-		cout << sum / N -1 << "\n";
+	int max = -4001;
+	int maxidx = 0;
+	int j = 0;
+	for (int i = 0; i < 8001; i++) {
+		if (num[i] > max) {
+			maxidx = i;
+			max = num[i];
+		}
+		else if (num[i] != 0 && j == 0 && num[i] == max) {
+			maxidx = i;
+			j++;
+		}
+	}
+	double mean;
+	if (sum > 0)
+		mean = (double)(sum / N);
 	else
-		cout << sum / N << "\n";
+		mean = ((double)(-1 * sum) / (double)N) * -1;
+	if (mean > -0.5 && mean < 0.5) mean = 0;
 
+	cout << round(mean) << "\n";
 	cout << v[(v.size() - 1) / 2] << "\n";
-
-	
+	cout << maxidx - 4000 << "\n";
 	cout << v.back() - v.front() << "\n";
 }
--- 2908.cpp
+++ 2908.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
-#include <array>
 #include <algorithm>
 using namespace std;
 
 int main()
 {
-	array <int, 3> num;
-	for (int i = 0; i < 3; i++)
-	{
-		cin >> num[i];
+	char num1[4], num2[4], tmp;
+
+	cin >> num1;
+	cin >> num2;
+
+	tmp = num1[0];
+	num1[0] = num1[2];
+	num1[2] = tmp;
+
+	tmp = num2[0];
+	num2[0] = num2[2];
+	num2[2] = tmp;
+
+	for (int i = 0; i < 4; i++) {
+		if (num1[i] > num2[i]) {
+			cout << num1[0] << num1[1] << num1[2] << endl;
+			break;
+		}
+		if (num1[i] < num2[i]) {
+			cout << num2[0] << num2[1] << num2[2] << endl;
+			break;
+		}
 	}
-	sort(num.begin(), num.end());
-	cout << num[1];
 }
