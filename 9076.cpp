de0bfe1efa72dcc577740884f5dd42d0e4388ffe
yeonggwangchoi <dudrhkd7811@naver.com>
2022-10-03 23:02:11 +0900

    221003 commit

---
+++ 9076.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	int T;
+	cin >> T;
+
+
+}
