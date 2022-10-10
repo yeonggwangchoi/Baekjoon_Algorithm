2d072b7a75f656fb9c651988128c1aadc8e42c0b
yeonggwangchoi <dudrhkd7811@naver.com>
2022-10-10 21:55:57 +0900

    221010 commit

---
+++ 11382.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	long long int A, B, C;
+	cin >> A >> B >> C;
+
+	cout << A + B + C;
+}
