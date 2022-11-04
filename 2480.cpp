b9324a999a5edf801c4d977967cb3cd3551b80c6
yeonggwangchoi <dudrhkd7811@naver.com>
2022-11-04 23:04:35 +0900

    221104 commit

--- 2480.cpp
+++ 2480.cpp
@@ -1,43 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-	int a, b, c, total;
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	ios::sync_with_stdio(false);
 
-	cin >> a;
-	cin >> b;
-	cin >> c;
+	int num[3];
+	int sum = 0;
+	int max = 0;
+	cin >> num[0] >> num[1] >> num[2];
 
-	if (a == b && b == c)
+	if (num[0] == num[1] && num[1] == num[2])
+		sum = 1000 * num[0] + 10000;
+	else if (num[0] != num[1] && num[1] != num[2] && num[2] != num[0])
 	{
-		total = a * 1000 + 10000;
+		for (int i = 0; i < 3; i++)
+		{
+			if (max < num[i])
+				max = num[i];
+		}
+		sum = max * 100;
 	}
-	else if (a == b || a == c)
+	else
 	{
-		total = a * 100 + 1000;
+		if (num[0] == num[1])
+			sum = num[0] * 100 + 1000;
+		else if (num[1] == num[2])
+			sum = num[1] * 100 + 1000;
+		else
+			sum = num[0] * 100 + 1000;
 	}
-	else if (b == c)
-	{
-		total = b * 100 + 1000;
-	}
-	else if (a != b && a != c && b != c)
-	{
-		if (a > b && b > c)
-			total = a * 100;
-		else if (a > b && c > b)
-			total = a * 100;
-		else if (b > a && a > c)
-			total = b * 100;
-		else if (b > c && c > a)
-			total = b * 100;
-		else if (c > a && a > b)
-			total = c * 100;
-		else if (c > b && b > a)
-			total = c * 100;
-	}
-
-	cout << total << endl;
-
-	return 0;
+	cout << sum;
 }
